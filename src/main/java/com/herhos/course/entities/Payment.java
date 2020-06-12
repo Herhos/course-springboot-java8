eeeae2c0fa436a14ed179850952c25fa4f2dcd8e
Marcio Herhos <herhos@outlook.com>
2020-06-12 18:54:02 -0300

    Subtotal & Total methods

--- src/main/java/com/herhos/course/entities/Payment.java
+++ src/main/java/com/herhos/course/entities/Payment.java
@@ -11,6 +11,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 //import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
@@ -26,6 +28,7 @@ public class Payment implements Serializable
 	//@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
