9b23309cfa37da92dbb8a769c48b1d288ebc097a
Marcio Herhos <herhos@outlook.com>
2020-06-13 20:53:23 -0300

    Exception handling - delete

--- src/main/java/com/herhos/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/herhos/course/resources/exceptions/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.herhos.course.services.exceptions.DatabaseException;
 import com.herhos.course.services.exceptions.ResourceNotFoundException;
 
 public class ResourceExceptionHandler
@@ -17,6 +18,15 @@ public class ResourceExceptionHandler
 	{
 		String error = "Recurso não encontrado!";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request)
+	{
+		String error = "Dado não encontrado!";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
