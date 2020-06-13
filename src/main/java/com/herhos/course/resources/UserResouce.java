9ee48ed8fa44d6baef8f2dc83d65fb7feeecec95
Marcio Herhos <herhos@outlook.com>
2020-06-13 10:53:46 -0300

    User delete

--- src/main/java/com/herhos/course/resources/UserResouce.java
+++ src/main/java/com/herhos/course/resources/UserResouce.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,13 @@ public class UserResouce
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
 			path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj); 
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id)
+	{
+		service.delete(id);
+		// noContent retorna uma resposta 204 do HTTP
+		return ResponseEntity.noContent().build();
 	}
 }
