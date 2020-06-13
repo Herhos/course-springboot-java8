9ac83946950ea6fc266dbd28023d432d31644c73
Marcio Herhos <herhos@outlook.com>
2020-06-13 15:52:49 -0300

    Exception handling - findById

--- src/main/java/com/herhos/course/services/UserService.java
+++ src/main/java/com/herhos/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.herhos.course.entities.User;
 import com.herhos.course.repositories.UserRepository;
+import com.herhos.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService
@@ -23,7 +24,8 @@ public class UserService
 	public User findById(Long id)
 	{
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+		// O método orElserThrow está gerando um erro com java 8
 	}
 	
 	public User insert(User obj)
